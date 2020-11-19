5c8e47f9d0af59414fcb517564019178a098d7fa
Milan Kocourek <milan.kocourek@movisio.com>
2020-11-19 09:36:35 +0100

    Fixed minimum copies for nested multipliers

--- src/Multiplier.php
+++ src/Multiplier.php
@@ -257,9 +257,9 @@ class Multiplier extends Container
 		}
 
 		// Default number of copies
-		if (!$this->isFormSubmitted() && !$this->values) {
+		if (!$this->values) {
 			$copyNumber = $this->copyNumber;
-			while ($copyNumber > 0 && $this->isValidMaxCopies()) {
+			while ($copyNumber > 0 && $this->isValidMaxCopies() && $this->totalCopies < $this->minCopies) {
 				$containers[] = $container = $this->addCopy();
 				$copyNumber--;
 			}
@@ -492,6 +492,7 @@ class Multiplier extends Container
 		if ($this->created) {
 			foreach ($this->getContainers() as $container) {
 				$this->removeComponent($container);
+				$this->totalCopies--;
 			}
 
 			$this->created = false;
